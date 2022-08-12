cdd03e5c38ad90b0de6928d091e1d4977af09f4f
yasahin <yasnsahinn@gmail.com>
2022-08-12 23:06:19 +0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// or less ideally
+import { Button, Table } from "react-bootstrap";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+
+      .get("https://restcountries.com/v2/all")
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.log({ error }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Capital</th>
+            <th>Region</th>
+            <th>Flag</th>
+          </tr>
+        </thead>
+        {countries
+          .filter((country) => {
+            console.log("Country" + country.capital);
+            if (search === "") {
+              return country;
+            } else if (
+              country.name.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return country;
+            }
+          })
+          .map((country, key) => {
+            return (
+              <tbody key={key}>
+                <tr style={{ width: "100%" }}>
+                  <td>{country.name}</td>
+                  <td>{country.capital}</td>
+                  <td>{country.region}</td>
+                  <td>
+                    <img
+                      style={{ width: "100px", backgroundColor: "black" }}
+                      src={country.flag}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+      </Table>
     </div>
   );
 }
